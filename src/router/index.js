21917a24c08f08980b9d1cb4ed0bd8f3ff540c7a
yuk7 <yukx00@gmail.com>
2019-12-01 14:29:41 +0900

    create NotFound page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Repos from '../views/Repos.vue'
+import NotFound from '../views/_NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/repos',
     name: 'repos',
     component: Repos
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
---
+++ src/views/_NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="nfpage">
+    <v-card min-width="270px">
+        <v-card-title>Page Not found<span>:(</span></v-card-title>
+        <v-card-text class="text--primary">
+            <div>The page you're looking for can't be found.</div>
+            <div>You may have mistyped the address or the page may have moved.</div>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn text to="/">Go to Top</v-btn>
+        </v-card-actions>
+    </v-card>
+</div>
+</template>
+
+<style>
+    .nfpage {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -webkit-transform: translateY(-50%) translateX(-50%);
+        transform: translateY(-50%) translateX(-50%);
+    }
+</style>
