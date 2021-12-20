889000b532a0cefef2e89f620d4e55fba16446df
yuk7 <yukx00@gmail.com>
2021-12-20 22:30:42 +0900

    App.vue: make responsive and bottom nav

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
     <v-app-bar elevation="4">
       <v-toolbar-title>yuk7.github.io</v-toolbar-title>
       <v-spacer />
-      <v-btn class="mr-3" to="/">Home</v-btn>
-      <v-btn class="mr-3" to="/works">Works</v-btn>
-      <v-btn class="mr-3" href="/blog">Blog</v-btn>
-      <v-btn class="mr-3" to="/repos">Repositories</v-btn>
+      <div class="d-none d-sm-flex">
+        <v-btn class="mr-3" to="/" @click.stop="drawer = false">Home</v-btn>
+        <v-btn class="mr-3" to="/works" @click.stop="drawer = false">Works</v-btn>
+        <v-btn class="mr-3" href="/blog" @click.stop="drawer = false">Blog</v-btn>
+        <v-btn class="mr-3" to="/repos" @click.stop="drawer = false">Repositories</v-btn>
+      </div>
       <v-btn icon @click="toggleTheme">
         <v-icon>mdi mdi-brightness-6</v-icon>
       </v-btn>
@@ -15,6 +17,28 @@
     <v-main>
       <router-view />
     </v-main>
+
+    <v-bottom-navigation class="d-flex d-sm-none">
+      <v-btn to="/">
+        <v-icon>mdi mdi-home</v-icon>
+        <span>Home</span>
+      </v-btn>
+  
+      <v-btn to="/works">
+        <v-icon>mdi mdi-briefcase</v-icon>
+        <span>Works</span>
+      </v-btn>
+  
+      <v-btn href="/blog">
+        <v-icon>mdi mdi-post</v-icon>
+        <span>Blog</span>
+      </v-btn>
+
+      <v-btn to="/repos">
+        <v-icon>mdi mdi-github</v-icon>
+        <span>Repos</span>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
@@ -22,14 +46,18 @@
   import { defineComponent } from 'vue'
 
   export type DataType = {
-    theme: string
+    theme: string,
+    drawer: boolean,
+    group: any
   }
 
   export default defineComponent({
     name: 'App',
     data(): DataType {
       return {
-        theme: "light"
+        theme: "light",
+        drawer: false,
+        group: null
       }
     },
     methods: {
