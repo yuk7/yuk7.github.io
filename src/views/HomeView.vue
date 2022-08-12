a66bcc6057c2446878d8161f719186f243deb943
yuk7 <yukx00@gmail.com>
2022-08-12 15:04:32 +0900

    fix card layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,34 @@
 <template>
   <v-container>
     <v-card class="mx-auto" elevation="4" max-width="500">
-      <v-list-item>
-        <v-list-item-content>
-          <h2>yuk7</h2>
-          <p>
-            <v-icon>mdi mdi-cake-variant</v-icon> Jan. 2000
-          </p>
-          <p>
-            <v-icon>mdi mdi-city</v-icon> Japan
-          </p>
-          <p>
-            <v-icon>mdi mdi-heart</v-icon> Linux/Android Udon/Chocolate/Mint
-          </p>
-          <p>
-            <v-icon>mdi mdi-code-braces</v-icon> C#/Go/Java/Kotlin/PHP/JavaScript
-          </p>
-        </v-list-item-content>
+      <v-card-title class="text-h5">yuk7</v-card-title>
+      <v-card-text class="py-0">
+        <v-row align="center" hide-gutters no-gutters>
+          <v-col cols="9">
+            <p>
+              <v-icon>mdi mdi-cake-variant</v-icon> Jan. 2000
+            </p>
+            <p>
+              <v-icon>mdi mdi-city</v-icon> Japan
+            </p>
+            <p>
+              <v-icon>mdi mdi-heart</v-icon> Linux/Android Udon/Chocolate/Mint
+            </p>
+            <p>
+              <v-icon>mdi mdi-code-braces</v-icon> C#/Go/Java/Kotlin/PHP/JavaScript
+            </p>
+          </v-col>
+          <v-col class="text-right">
+            <v-avatar class="profile" color="grey" size="60%">
+              <v-img src="https://avatars2.githubusercontent.com/u/29954265" max-width="200px"></v-img>
+            </v-avatar>
+          </v-col>
+        </v-row>
+      </v-card-text>
 
-        <v-spacer />
+      <v-spacer />
 
-        <v-avatar class="profile" color="grey" size="20%">
-          <v-img src="https://avatars2.githubusercontent.com/u/29954265" max-width="200px"></v-img>
-        </v-avatar>
-      </v-list-item>
+
 
       <v-card-actions>
         <v-btn icon @click="dialog_email = true">
@@ -53,8 +58,7 @@
         <v-list-item prependIcon="mdi mdi-package-variant" title="Container Techs" subtitle="Docker, LXC, nspawn" />
         <v-list-item prependIcon="mdi mdi-web" title="Web Development" subtitle="JavaScript, PHP, Java" />
         <v-list-item prependIcon="mdi mdi-android" title="Android App Development" subtitle="Java, Kotlin, Cordova" />
-        <v-list-item prependIcon="mdi mdi-dot-net" title=".NET Development"
-          subtitle="C#, VB.NET, WinForms, WPF" />
+        <v-list-item prependIcon="mdi mdi-dot-net" title=".NET Development" subtitle="C#, VB.NET, WinForms, WPF" />
       </v-list>
     </v-card>
 
@@ -105,50 +109,50 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import HistoryTimeLine from "@/components/HistoryTimeLine.vue";
+import { defineComponent } from 'vue';
+import HistoryTimeLine from "@/components/HistoryTimeLine.vue";
 
-  export default defineComponent({
-    components: {
-      HistoryTimeLine
-    },
-    data() {
-      return {
-        dialog_email: false,
-        dialog_btc: false,
-        history: [
-          {
-            year: '2013~2015',
-            title: 'Robotics Contest Club Activity in JHS',
-            content: 'Participated in some robotics contests as a club activity. Experienced as the technical leader and programmer in the team. Our team won a special award in a area tournament.',
-            env: 'C, C#, AVR, STM32'
-          },
-          {
-            year: '2015~2018',
-            title: 'Teaching Assistant in SHS',
-            content: 'Teaching Assistant in C language and JavaScript classes.',
-            env: 'C, JavaScript'
-          },
-          {
-            year: '2018~',
-            title: 'Computer Science Student in a College',
-            content: 'Learned programming and computer science in college. Joined a programming club and some hackathon. My research&production work won a special incentive award on campus.',
-            env: 'C#, Java, PHP, Python, R, WPF, Servlet, JSP, Spring'
-          },
-          {
-            year: '2019~2021',
-            title: 'Part-time backend and frontend developer in a Web company',
-            content: 'Joined a web company and some project. Developed some hybrid apps, web apps and sites.',
-            env: 'PHP, JavaScript, Cordova, Bash, Python'
-          }
-        ],
-      }
+export default defineComponent({
+  components: {
+    HistoryTimeLine
+  },
+  data() {
+    return {
+      dialog_email: false,
+      dialog_btc: false,
+      history: [
+        {
+          year: '2013~2015',
+          title: 'Robotics Contest Club Activity in JHS',
+          content: 'Participated in some robotics contests as a club activity. Experienced as the technical leader and programmer in the team. Our team won a special award in a area tournament.',
+          env: 'C, C#, AVR, STM32'
+        },
+        {
+          year: '2015~2018',
+          title: 'Teaching Assistant in SHS',
+          content: 'Teaching Assistant in C language and JavaScript classes.',
+          env: 'C, JavaScript'
+        },
+        {
+          year: '2018~',
+          title: 'Computer Science Student in a College',
+          content: 'Learned programming and computer science in college. Joined a programming club and some hackathon. My research&production work won a special incentive award on campus.',
+          env: 'C#, Java, PHP, Python, R, WPF, Servlet, JSP, Spring'
+        },
+        {
+          year: '2019~2021',
+          title: 'Part-time backend and frontend developer in a Web company',
+          content: 'Joined a web company and some project. Developed some hybrid apps, web apps and sites.',
+          env: 'PHP, JavaScript, Cordova, Bash, Python'
+        }
+      ],
     }
-  });
+  }
+});
 </script>
 
 <style>
-  .mx-auto {
-    margin: 10px auto;
-  }
+.mx-auto {
+  margin: 10px auto;
+}
 </style>
