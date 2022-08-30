84a56c94c3d2da0044e1c3b149f45ed414e9253f
John Carlo Austria <johncarloaustria@proton.me>
2022-08-30 20:34:28 +0800

    New link for my personal site

--- src/views/FriendsLinkView.vue
+++ src/views/FriendsLinkView.vue
@@ -7,7 +7,7 @@
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/62464927" title="LapisApple" href="https://laple.me/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/2550010" title="hdk5" href="https://hdk5.xyz/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/76040821" title="bagussona" href="http://bagussona.github.io/"/>
-        <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/64476715" title="jaycedotbin" href="http://jaycedotbin.vercel.app/"/>
+        <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/64476715" title="jaycedotbin" href="http://jaycedotbin.github.io/"/>
       </v-list>
     </v-card>
   </v-container>
