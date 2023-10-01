73d835125126bd3e41eb3d38877497679578ecbc
yuk7 <yukx00@gmail.com>
2023-10-01 22:56:01 +0900

    update FriendsLinkView.vue

--- src/views/FriendsLinkView.vue
+++ src/views/FriendsLinkView.vue
@@ -5,6 +5,7 @@
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/12298476" title="justforlxz" href="https://blog.justforlxz.com"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/13084946" title="hwdef" href="http://www.hwdef.org/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/62464927" title="LapisApple" href="https://laple.me/"/>
+        <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/19504193" title="AzureZeng" href="https://azurezeng.com/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/2550010" title="hdk5" href="https://hdk5.xyz/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/76040821" title="bagussona" href="http://bagussona.github.io/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/64476715" title="jaycedotbin" href="http://jaycedotbin.github.io/"/>
