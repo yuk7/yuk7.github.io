1eae988a222323549d5e08f9f30a14be48afaf8c
yuk7 <yukx00@gmail.com>
2023-10-03 08:09:35 +0900

    update FriendsLinkView.vue

--- src/views/FriendsLinkView.vue
+++ src/views/FriendsLinkView.vue
@@ -6,6 +6,7 @@
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/13084946" title="hwdef" href="http://www.hwdef.org/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/62464927" title="LapisApple" href="https://laple.me/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/19504193" title="AzureZeng" href="https://azurezeng.com/"/>
+        <v-list-item prepend-avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7WYVq2Nb9fDK1hr0ch8w2--iR1WVVoxYdfK-Rm34q90sIR3fc" title="joexyz" href="https://joexyz.online/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/2550010" title="hdk5" href="https://hdk5.xyz/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/76040821" title="bagussona" href="http://bagussona.github.io/"/>
         <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/64476715" title="jaycedotbin" href="http://jaycedotbin.github.io/"/>
